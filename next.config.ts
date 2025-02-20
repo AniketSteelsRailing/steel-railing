066e721d747a46db7af756fdb6744129d4e34721
aniketgosavi1997 <aniketgosavi0987@gmail.com>
2025-02-21 00:12:48 +0530

    Updated config filw

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,25 @@
-// // next.config.mjs
-/** 
- * @type {import('next').NextConfig}
- */
-const nextConfig = {
+import { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
   // output: 'export',
   images: {
-            remotePatterns: [
-              {
-                protocol: 'https',
-                hostname: 'fastly.picsum.photos',
-                port: '',
-                pathname: '/id/**', // Match all paths starting with "/id/"
-              },
-            ],
-          }
+                remotePatterns: [
+                  {
+                    protocol: 'https',
+                    hostname: 'fastly.picsum.photos',
+                    port: '',
+                    pathname: '/id/**', // Match all paths starting with "/id/"
+                  },
+                ],
+              }
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
 }
- 
-export default nextConfig
+
+export default nextConfig;

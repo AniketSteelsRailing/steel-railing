4f307ef5fea211ae5774340e8999b798cc44010c
aniketgosavi1997 <aniketgosavi0987@gmail.com>
2025-02-24 20:25:57 +0530

    UPdated code for seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,26 +3,24 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header/header";
 import Footer from "@/components/Footer/footer";
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-// const inter = Inter({ subsets:["latin"]});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-const pageTitle = "Modern Steel Railing";
 export const metadata: Metadata = {
-  title : `${pageTitle}`,
+  title : "Modern Steel Railing",
   description: "Enhance your space with premium steel railings, custom window and gate designs. Offering unmatched strength, safety, and sleek aesthetics for every project. Built to last, crafted to impress.",
   twitter: {
     card:"summary_large_image"
   }
 };
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
 
 export default function RootLayout({
   children,

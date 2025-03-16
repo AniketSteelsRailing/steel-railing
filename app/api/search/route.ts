cb550abe26b359c0c5d5b0e4c61fa7fbe650468e
aniketgosavi1997 <aniketgosavi0987@gmail.com>
2025-03-16 14:31:58 +0530

    Added error in catch

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -30,7 +30,7 @@ export async function GET(req: NextRequest) {
     return NextResponse.json(data, { status: 200 });
   } catch (error) {
     return NextResponse.json(
-      { error: "Failed to fetch data" },
+      { error: error },
       { status: 500 }
     );
   }
